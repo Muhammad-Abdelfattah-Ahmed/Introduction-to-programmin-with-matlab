76b86aae4924d416becb4919b92e40636aad2ed9
Muhammad Abd El Fattah Ahmed <87549698+Muhammad-Abdelfattah-Ahmed@users.noreply.github.com>
2022-10-09 03:13:16 +0200

    Slove week 1 assignment

---
+++ Week 1/Calculation_of_debt.m
@@ -0,0 +1,5 @@
+Borrowed_Money = 1000 ;
+annual_interest = 0.1 ;
+loan_after_first_year = ( Borrowed_Money * annual_interest ) + Borrowed_Money
+debt = ( loan_after_first_year * annual_interest ) + loan_after_first_year
+
